d54772c54707d5438cf7a4dce8d3a55a7e0797fa
anurag <anurag.botre.202@gmail.com>
2020-01-12 21:56:37 +0530

    Stream reduce example.

--- src/main/java/com/trendcore/learning/apache/spark/Main.java
+++ src/main/java/com/trendcore/learning/apache/spark/Main.java
@@ -1,4 +1,28 @@
 package com.trendcore.learning.apache.spark;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Main {
+
+    public static void main(String[] args) {
+        HashMap<Integer, Integer> reduce = Arrays.asList(1, 1, 2, 3, 5, 2, 3, 4, 5).stream()
+                .map(integer -> integer.intValue())
+                .reduce(new HashMap<>(), (hashMap, integer) -> {
+                    hashMap.computeIfAbsent(integer, o -> 0);
+                    hashMap.compute(integer, (o, o2) -> o2 + 1);
+                    System.out.println("accumulating");
+                    return hashMap;
+                }, (hashMap, hashMap2) -> {
+                    //combiner is called only during in case of parallel stream.
+                    hashMap.putAll(hashMap2);
+                    System.out.println("combining");
+                    return hashMap;
+                });
+
+        reduce.entrySet().stream().forEach(o -> {
+            System.out.println(o.getKey() + " " + o.getValue());
+        });
+    }
+
 }
